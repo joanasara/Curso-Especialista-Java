3cbc3cf55447d38a0255f0249c5b0743c6c5103d
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2023-10-12 12:10:45 -0300

    Terceira aula do modulo Fudamentos java

---
+++ fundamentos/ConversaoCodigo.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class ConversaoCodigo {
+    public static void main(String[] args) {
+
+        System.out.println("Conversao de codigo");
+
+        // precisa ter uma otima indentacao no codigo
+        // saber trabalhar com espacos
+        // precisa saber a regra
+
+    }
+}
