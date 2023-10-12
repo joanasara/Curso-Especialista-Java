13f7ff4c62508f7808419eec35475d0282bf5a0e
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2023-10-12 11:11:53 -0300

    Segunda aula do modulo Fudamentos java

---
+++ fundamentos/EscrevendoComentarios.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class EscrevendoComentarios {
+    /*
+        pode ser escrido qualquer comentarios nesse bloco de comentarios
+     */
+    public static void main(String[] args) { //ou na frente de uma declara ao ou instrucao
+
+        // imprime a mensagem na saida padrao
+        System.out.println("Escrevendo comentarios");
+
+
+    }
+}
