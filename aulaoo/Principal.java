c05ac14e99b92706d37dd806928873a4e9f27bab
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2024-05-26 12:01:11 -0300

    Atribuindo o objeto na composição

--- aulaoo/Principal.java
+++ aulaoo/Principal.java
@@ -2,18 +2,17 @@ package aulaoo;
 
 public class Principal {
     public static void main(String[] args) {
+       Pessoa eu = new Pessoa();
+       eu.anoNascimento = "1998";
+       eu.cpf = "3454566756";
+       eu.nome = "joao";
 
         Carro meuCarro = new Carro();
         meuCarro.anoFabricacao = 2010;
         meuCarro.cor = "preto";
         meuCarro.fabricante = "Fiat";
         meuCarro.modelo = "SU-V";
-
-        Carro seuCarro = new Carro();
-        seuCarro.anoFabricacao = 2022;
-        seuCarro.cor = "Cinza";
-        seuCarro.fabricante = "Toyota";
-        seuCarro.modelo = "Corolla";
+        meuCarro.propietario = eu;
 
         System.out.println("Meu Carro");
         System.out.println("------------");
@@ -22,13 +21,5 @@ public class Principal {
         System.out.printf( "Modelo: %s%n",  meuCarro.modelo);
         System.out.printf( "cor: %s%n",  meuCarro.cor);
 
-        System.out.println();
-
-        System.out.println("Seu Carro");
-        System.out.println("------------");
-        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
-        System.out.printf("Fabricante: %s%n", seuCarro.fabricante);
-        System.out.printf( "Modelo: %s%n",  seuCarro.modelo);
-        System.out.printf( "cor: %s%n",  seuCarro.cor);
     }
 }
